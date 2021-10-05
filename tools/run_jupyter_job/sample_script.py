2bd2eade8626bff339d7fc572697834c48c5a5bd
Anup Kumar <anup.rulez@gmail.com>
2021-10-05 09:00:40 +0200

    update tool and confs

---
+++ tools/run_jupyter_job/sample_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#import h5py 
+#import galaxy-ie-helpers as gie
+
+def print_script():
+    print("Found script")
+    #print(gie.__version__)
+    #print(h5py.__version__)
+
+
+if __name__ == "__main__":
+    print_script()
